87947ae6cd41a552ed9c1e872078add244fef521
bfrasure <bill.frasure@gmail.com>
2019-04-02 18:15:54 -0600

    Theoretically enable live payments to my company account :O

--- Makefile
+++ Makefile
@@ -4,3 +4,4 @@ build:
 	# go get ./...
 	go build -o functions/movies-lambda  ./lambdas
 	go build -o functions/submit-stripe-charge  ./lambdas/stripe
+	go build -o functions/live-submit-stripe-charge  ./lambdas/stripe/live
--- lambdas/stripe/live/LivePayments.go
+++ lambdas/stripe/live/LivePayments.go
@@ -18,7 +18,7 @@ func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResp
 	m, _ := url.ParseQuery(request.Body)
 	token := m.Get("stripeToken")
 
-	stripe.Key = os.Getenv("STRIPE_SECRET_KEY")
+	stripe.Key = os.Getenv("LIVE_STRIPE_SECRET_KEY")
 
 	// Token is created using Checkout or Elements!
 	// Get the payment token ID submitted by the form:
